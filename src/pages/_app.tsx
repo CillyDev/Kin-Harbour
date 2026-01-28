85d29a1ac7c6d88459e42fb1bb08cca7cfaee172
CillyDev <abdulaziz.nabaa@gmail.com>
2026-01-28 13:28:27 +0100

    fixed the searchbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,11 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className="grid w-full h-full grid-rows-[repeat(auto-fill,10px)] grid-cols-[repeat(auto-fill,10px)] overflow-x-hidden">
-      <div className="grid row-start-[1] row-end-[8] col-start-[1] col-end-[80] p-2">
+    <main className="grid w-full h-full grid-cols-24 grid-rows-24">
+      <div className="grid row-start-1 row-end-3 col-start-1 col-end-16 p-2 justify-center">
         <MainNavigation />
       </div>
-      <div className="grid h-full w-full row-start-[1] row-end-[8] col-start-[80] col-end-[120] p-2">
+      <div className="grid h-full w-full row-start-1 row-end-3 col-start-16 col-end-22 p-2">
         <SearchBar />
       </div>
       <div className="w-full h-full grid row-start-[8] row-end-[-1] col-start-[-1] col-end-[1]">
